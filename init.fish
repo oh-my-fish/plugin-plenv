66584e746dbe3e41c89af9d5f9f6bdbe8bfc2ae8
Justin Hileman <justin@justinhileman.info>
2016-03-25 00:09:12 -0700

    Only add missing paths during init.
    This prevents issues when reloading the shell. See oh-my-fish/plugin-composer#7

--- init.fish
+++ init.fish
@@ -1,3 +1,9 @@
 set -q PLENV_ROOT; or set -l PLENV_ROOT $HOME/.plenv
 
-set PATH $PLENV_ROOT/shims $PLENV_ROOT/bin $PATH
+if not contains $PLENV_ROOT/bin $PATH
+  set PATH $PLENV_ROOT/bin $PATH
+end
+
+if not contains $PLENV_ROOT/shims $PATH
+  set PATH $PLENV_ROOT/shims $PATH
+end
